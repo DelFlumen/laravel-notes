3126c3b370739ea454c7abc70971be98b4dd24b0
Yaroslav Zaychenko <delflumen@ukr.net>
2024-04-26 15:36:47 +0300

    feat: implement update note

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -57,7 +57,13 @@ class NoteController extends Controller
      */
     public function update(Request $request, Note $note)
     {
-        return 'update';
+        $data = $request->validate([
+            'note' => ['required', 'string']
+        ]);
+
+        $note->update($data);
+
+        return to_route('note.show', $note)->with('message', 'Note was updated');
     }
 
     /**
